38c236dbe824ac78f36b64ff40ab20319b698dcc
amdiag <amdiag>
2019-07-07 20:41:27 +0300

    upd. String.truncate()

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,2 +1,10 @@
 package ru.skillbranch.devintensive.extensions
 
+import java.util.*
+
+public fun String.truncate(numDelimit: Int = 16): String {
+    if (this.length <= numDelimit)
+        return this.trimEnd()
+    else
+        return "${this.subSequence(0, numDelimit).trimEnd().toString()}..."
+}
